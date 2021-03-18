029b6807ce0b0c199b615d295db91a78280d709a
MickaelGoyat <mickael22960@gmail.com>
2021-03-18 11:08:20 +0100

    Installation WebPack + Non finition du formulaire de login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,19 +6,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
- * @Route("/login", name="login")
+ * @Route("/", name="login")
  */
 class LoginController extends AbstractController
 {
-    /**
-     * @Route("/", name="_index")
-     */
-    public function index(): Response
+    #[Route('/', name: 'user_login')]
+    public function login(AuthenticationUtils $utils): Response
     {
         return $this->render('login/index.html.twig', [
-            'title' => 'Login',
+            'controller_name' => 'LoginController',
+            'loginError' => $utils->getLastAuthenticationError(),
+            'loginUsername' => $utils->getLastUsername(),
         ]);
+    }
+
+    #[Route('/logout', name: 'user_logout')]
+    public function logout()
+    {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
